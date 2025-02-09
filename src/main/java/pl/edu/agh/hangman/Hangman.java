0506e5bf729919d63dd53de59263a389bc456442
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2025-02-09 14:29:50 +0100

    hangman

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.hangman;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -54,16 +58,46 @@ public class Hangman {
                     "========"
     };
 
+
+
+
     public static void main(String[] args) {
 
-        Word w = new Word();
-        w.loadTxtFile();
-        w.pickWord();
-        String out = w.getOutputWord();
-        System.out.println(w.getWord());
-        System.out.println(out);
-        boolean k = w.checkIfLetterInWord('e');
-        System.out.println(w.getWord());
-        System.out.println(w.getOutputWord());
-    }
+        // pamietac na jakim jest znaku
+        // zwracac albo krok animacji -
+        // jezeli true to zwraca ostani
+        // jezeli false kolejny krok
+        // jezeli false i wyjatek to exeption
+
+
+        String word = "JAVA";
+        char targetChar = 'A';
+        int count = 0;
+
+        // Iteracja przez znaki w słowie
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == targetChar) {
+                List<String> words = Collections.singletonList(HANGMANPICS[0]);
+
+                System.out.println(words.toString());
+            }else {
+                List<String> words = Collections.singletonList(HANGMANPICS[i]);
+                System.out.println(words.toString());
+                count++;
+            }
+        }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
 }
